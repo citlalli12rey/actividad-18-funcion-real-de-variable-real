e9eb6bc395afa0c02fabccd78a7753477d8b717f
Diana Citlalli <91552544+citlalli12rey@users.noreply.github.com>
2021-10-28 14:41:09 -0500

    Add files via upload

---
+++ ejercicio4.m
@@ -0,0 +1,19 @@
+%Scrip Octave
+%title          :Actividad 18 Función de Variable Real
+%Descripcion    :Función real de variable real y su representación gráfica.
+%Autor          :Diana Citlali  Espinosa Reyes  
+%Date           :202123199
+%Versión        :4
+
+% representar la siguiente ecuación
+% fx = ( x . ^ 2 ) + ( 6 * ( x ));
+claro
+paquete de  carga simbólico
+syms  x
+x = [- 10 : 0,1 : 4 ];
+y = x . ^ 2 + 6 * x
+trama ( x , y )
+cuadrícula  en
+ylabel ( ' y ' )
+xlabel ( ' x ' )
+title ( ' raices (-6,0) (0,0), dominio xE R, rango yE [-9, + oo), minimo (-3, -9) ' 
